bd21a97f6aa5c694cf792795662a679384dcd98c
andresdv10 <andresdv10@gmail.com>
2026-01-30 19:16:20 -0500

    Update Supabase ANON_KEY value

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 export const SUPABASE_URL = "https://dzncmwtgeaddgzrcdpaj.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_Ee9asI0KG4LZ9jkCGb3uow_ueRP0r3s";
+export const SUPABASE_ANON_KEY = "sb_publishable_Ee9asI0KG4LZ9jkCGb3uow_ueRPOr3s";
 
 export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
